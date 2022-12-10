232e358e655214c4afe43b47f71cacc00f70e1e9
PjotrPiet <Pjotrpiet@outlook.com>
2022-12-10 07:18:12 +0100

    updated so that both parts are actually working

--- 2022/dag10.py
+++ 2022/dag10.py
@@ -1,15 +1,28 @@
 import parser
 
 def deel1(lines):
-    pass
+    cycle = 0
+    reg1 = 1
+    vals = []
+
+    for line in lines:
+        line = line.split(' ')
+        cycle += 1
+
+        if cycle == 20 or (cycle - 20) % 40 == 0:
+            vals.append(reg1 * cycle)
+
+        if line[0] == 'addx':
+            cycle += 1
+
+            if cycle == 20 or (cycle - 20) % 40 == 0:
+                vals.append(reg1 * cycle)
+
+            reg1 += int(line[1])
+
+    print(sum(vals))
 
 def deel2(lines):
-    pass
-
-def main():
-    lines = parser.input_as_lines('inputs/dag10.txt')
-    # lines = parser.input_as_lines('inputs/dag10_test.txt')
-
     cycle = 0
     reg1 = 1
     screen = []
@@ -46,6 +59,14 @@ def main():
 
     for row in screen:
         print(row)
+
+
+def main():
+    lines = parser.input_as_lines('inputs/dag10.txt')
+    # lines = parser.input_as_lines('inputs/dag10_test.txt')
+
+    deel1(lines)
+    deel2(lines)
 
 
 if __name__ == "__main__":
