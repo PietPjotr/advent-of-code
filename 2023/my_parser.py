48744a9ef16546156a34d6558e3f8dec518ea23e
PjotrPiet <Pjotrpiet@outlook.com>
2023-12-19 17:01:54 +0100

    parser verbetering

--- 2023/my_parser.py
+++ 2023/my_parser.py
@@ -2,7 +2,7 @@ from typing import List
 def input_split_by_emtpy_newline(filename: str) -> str:
     """Returns the content of the input file as groups split by empty newline"""
     lines = [el if el != '' else ' ' for el in input_as_lines(filename)]
-    return [[el for el in part] for part in ''.join(lines)]
+    return [[el for el in part.split()] for part in '\n'.join(lines).split(' ')]
 
 
 def input_as_string(filename: str) -> str:
