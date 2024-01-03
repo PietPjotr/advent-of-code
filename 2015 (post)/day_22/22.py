d54665ac903c57fcb4ef66907fb6771cf2812b42
PjotrPiet <Pjotrpiet@outlook.com>
2024-01-04 00:22:15 +0100

    2015 af bitchessss

--- 2015 (post)/day_22/22.py
+++ 2015 (post)/day_22/22.py
@@ -23,44 +23,49 @@ def cast_spell(player, spell_id, active):
     return ret_player, ret_active
 
 
-def apply(player, boss, active, turn=''):
+def apply(player, boss, active):
+    ids = [i for i, timer in active]
+
+    # reset the player shield (this mistake took about 3 hours to find)
+    player[2] = 0
+
     for spell_id, timer in active:
-        stats = spells[spell_id]
-        player[0] += stats[1]
-        player[1] += stats[2]
-        if player[2] == 0:
-            player[2] += stats[3]
+        cost, hp5, mp5, armor, dmg, _ = spells[spell_id]
+        player[0] += hp5
+        player[1] += mp5
+        player[2] += armor
 
-        boss[0] -= stats[4]
-        timer -= 1
+        boss[0] -= dmg
 
+    # update and remove spells
     active = [(spell_id, timer - 1) for spell_id, timer in active]
-    active = [spell for spell in active if spell[-1] > 0]
+    active = [(spell_id, timer) for spell_id, timer in active if timer > 0]
 
     return player, boss, active
 
 
 def boss_attack(player, boss):
-    player[0] -= max(1, boss[1] - player[2])
-    return player, boss
+    php, pmp, parmor = player
+    bhp, bdmg = boss
+    php -= max(1, bdmg - parmor)
+    return [php, pmp, parmor], boss
 
 
-def solve():
+def solve(hardmode=False):
     base_player = [50, 500, 0]
     boss = [71, 10]
 
-    base_player = [10, 250, 0]
-    boss = [13, 8]
-
     min_mana = float('inf')
     min_casted = []
-    player_stack = [(base_player, boss, [], 0, [])]
+    player_stack = [(base_player, boss, [], 0)]
     while player_stack:
         boss_stack = []
 
         # players turn
-        for i, frame in enumerate(player_stack):
-            player, boss, active, mana_spent, casted = frame
+        for frame in player_stack:
+            player, boss, active, mana_spent = frame
+            if hardmode:
+                player[0] -= 1
 
             player, boss, active = apply(player, boss, active)
 
@@ -71,39 +76,36 @@ def solve():
             if boss[0] <= 0:
                 if mana_spent < min_mana:
                     min_mana = mana_spent
-                    min_casted = deepcopy(casted)
-                    print(min_mana)
                 continue
 
             # players turn: cast all possible spells and simulate
-            ids = [i for i, timer in active]
-            for i in range(len(spells)):
-                if i in ids:
+            active_spell_ids = [i for i, timer in active]
+            for i in [0, 1, 2, 3, 4]:
+                if i in active_spell_ids:
                     continue
+
                 cost = spells[i][0]
                 if cost > player[1]:
                     continue
 
                 np, na = cast_spell(player, i, active)
-                boss_stack.append((np, deepcopy(boss), na, mana_spent + cost, casted + [i]))
+                boss_stack.append((np, deepcopy(boss), na, mana_spent + cost))
 
             # not enough mana, player lost
             if not boss_stack:
                 continue
 
         player_stack = []
-        for i, frame in enumerate(boss_stack):
+        for frame in boss_stack:
 
-            player, boss, active, mana_spent, casted = frame
+            player, boss, active, mana_spent = frame
 
             player, boss, active = apply(player, boss, active)
 
             # boss died
             if boss[0] <= 0:
-                if mana_spent < min_mana:
+                if mana_spent <= min_mana:
                     min_mana = mana_spent
-                    min_casted = deepcopy(casted)
-                    print(min_mana)
                 continue
 
             player, boss = boss_attack(player, boss)
@@ -112,14 +114,10 @@ def solve():
             if player[0] <= 0:
                 continue
 
-            player_stack.append((player, boss, active, mana_spent, casted))
+            player_stack.append((player, boss, active, mana_spent))
 
-    return min_mana, casted
+    return min_mana
 
-res, casted = solve()
-print([spell_names[i] for i in casted])
-spells_names = [spell_names[i] for i in casted]
-print(sum([spells[spell][0] for spell in casted]))
 
-casted_small = ['Poison', 'Magic Missile']
-print(sum([spells[spell_names.index(spell)][0] for spell in casted_small]))
+print(solve())
+print(solve(True))
