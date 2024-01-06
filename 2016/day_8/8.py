f65996f509a380737c1905a9c9069a52cfdadbae
PjotrPiet <Pjotrpiet@outlook.com>
2024-01-06 22:13:59 +0100

    deze was echt leuk, ook leuk dat er daadwerkelijk een code ontstond van het volgen van instructies en het aanzetten van rects op een scherm 5/5

---
+++ 2016/day_8/8.py
@@ -0,0 +1,53 @@
+import sys
+sys.path.append('..')
+import my_parser as p
+from copy import deepcopy
+
+L = p.input_as_lines('inputs/inp.txt')
+G = G = [[el for el in line] for line in L]
+R = len(G)
+C = len(G[0])
+
+def show(grid):
+    for row in grid:
+        for el in row:
+            print(el, end='')
+        print()
+    print()
+
+C = 50
+R = 6
+grid = [['.' for c in range(C)] for r in range(R)]
+
+for line in L:
+    next_grid = deepcopy(grid)
+    args = line.split()
+    if args[0] == 'rect':
+        width, height = args[1].split('x')
+        width, height = int(width), int(height)
+        for c in range(width):
+            for r in range(height):
+                next_grid[r][c] = '#'
+    elif args[1] == 'row':
+        r = int(args[2].split('=')[1])
+        shift = int(args[-1])
+        for c in range(C):
+            next_grid[r][(c + shift) % C] = grid[r][c]
+    elif args[1] == 'column':
+        c = int(args[2].split('=')[1])
+        shift = int(args[-1])
+        for r in range(R):
+            next_grid[(r + shift) % R][c] = grid[r][c]
+
+    grid = next_grid
+
+show(grid)
+
+p1 = 0
+for row in grid:
+    for el in row:
+        if el == '#':
+            p1 += 1
+
+
+print(p1)
