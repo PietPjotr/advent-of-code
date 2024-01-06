0147e22b876116deff6d6d500b838b9f93bc04f5
PjotrPiet <Pjotrpiet@outlook.com>
2024-01-06 14:07:41 +0100

    lezen is echt moeilijk, verder wel weer een leuke vraag! Lijkt heel erg op een van 2015

--- 2016/day_5/5.py
+++ 2016/day_5/5.py
@@ -1,8 +1,23 @@
-import sys
-sys.path.append('..')
-import my_parser as p
+import hashlib
 
-L = p.input_as_lines('inputs/inp.txt')
-G = G = [[el for el in line] for line in L]
-R = len(G)
-C = len(G[0])
+
+def solve():
+    code1 = ''
+    code2 = ['-' for _ in range(8)]
+    inp = 'wtnhxymk'
+    i = 0
+    while len(code1) < 8 or '-' in code2:
+        to_hash = inp + str(i)
+        hashed = hashlib.md5(to_hash.encode()).hexdigest()
+        if hashed.startswith('00000'):
+            code1 += hashed[5]
+            if hashed[5].isdigit():
+                index = int(hashed[5])
+                if 0 <= index <= 7 and code2[index] == '-':
+                    code2[index] = hashed[6]
+        i += 1
+    print(code1[:8])
+    print(''.join(code2))
+
+
+solve()
