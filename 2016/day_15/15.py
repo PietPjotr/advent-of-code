2981793b2341542203a520bb82feecef5f9f6ca1
PjotrPiet <Pjotrpiet@outlook.com>
2024-01-09 12:10:22 +0100

    geinig

--- 2016/day_15/15.py
+++ 2016/day_15/15.py
@@ -3,10 +3,6 @@ sys.path.append('..')
 import my_parser as p
 
 L = p.input_as_lines('inputs/inp.txt')
-G = G = [[el for el in line] for line in L]
-R = len(G)
-C = len(G[0])
-
 discs = []
 for line in L:
     line = line.replace('.', '')
