9dd7a511282b14f739f888f561b4a649a21fd799
PjotrPiet <Pjotrpiet@outlook.com>
2024-01-10 16:02:42 +0100

    dag 22 af bby, duurde lang maar was wel echt satisfying en leuk om te doen

--- 2016/day_22/22.py
+++ 2016/day_22/22.py
@@ -21,14 +21,12 @@ for node1 in nodes:
         avail = node2[3]
         if node1 != node2 and used != 0 and used <= avail:
             p1 += 1
+print(p1)
 
-# print(p1)
-
-avail = sorted([node[3] for node in nodes], reverse=True)
 R = max([node[0][1] for node in nodes]) + 1
 C = max([node[0][0] for node in nodes]) + 1
 
-max_avail = avail[0]
+max_avail = max([node[3] for node in nodes])
 graph = [['-' for c in range(C)] for r in range(R)]
 start = (0, 0)
 for node in nodes:
@@ -41,25 +39,11 @@ for node in nodes:
         start = node[0][::-1]
 
 
-def show(graph, pos, target):
-    for r, row in enumerate(graph):
-        for c, node in enumerate(row):
-            if (r, c) == target:
-                print('G', end=' ')
-            elif (r, c) == pos:
-                print('-', end=' ')
-            else:
-                print(node, end=' ')
-        print()
-    print()
-
-
 target = (0, C - 1)
 stack = [[0 + C - 1, 0, start, target]]
 DP = set((start, target))
 while stack:
-    # Use heapq to pop the node with the smallest priority
-    _, steps, pos, target = heapq.heappop(stack)
+    p, steps, pos, target = heapq.heappop(stack)
     if (pos, target) in DP:
         continue
     DP.add((pos, target))
@@ -76,6 +60,6 @@ while stack:
             # check if the target gets swapped
             if npos == target:
                 ntarget = deepcopy(pos)
+                p = target[0] + target[1]
 
-            # Use heapq to push the node with the updated priority
-            heapq.heappush(stack, (target[0] + target[1], steps + 1, npos, ntarget))
+            heapq.heappush(stack, (p, steps + 1, npos, ntarget))
