cc8dc7e37abb11b6fefad58646ec70c8e147f72d
PjotrPiet <Pjotrpiet@outlook.com>
2024-01-14 15:30:28 +0100

    ezpz zeker wel een leuke tho

--- 2017/day_16/16.py
+++ 2017/day_16/16.py
@@ -1,8 +1,42 @@
 import sys
 sys.path.append('..')
 import my_parser as p
+import re
 
-L = p.input_as_lines('inputs/inp.txt')
-G = G = [[el for el in line] for line in L]
-R = len(G)
-C = len(G[0])
+L = p.input_as_lines('inputs/inp.txt')[0]
+inss = L.split(',')
+l = list('abcdefghijklmnop')
+
+loop = ['0']
+i = 0
+while i < 100:
+    for ins in inss:
+        c = ins[0]
+        if c == 's':
+            n = int(ins[1:])
+            l = l[-n:] + l[:-n]
+        elif c == 'x':
+            a, b = [int(el) for el in re.findall(r'\d+', ins)]
+            temp = l[a]
+            l[a] = l[b]
+            l[b] = temp
+        elif c == 'p':
+            na, nb = re.findall(r'[a-z]+', ins[1:])
+            a = l.index(na)
+            b = l.index(nb)
+            temp = l[a]
+            l[a] = l[b]
+            l[b] = temp
+
+    state = ''.join(l)
+    if state in loop:
+        repeat = i
+        break
+    else:
+        loop.append(state)
+    i += 1
+
+
+print(loop[1])
+loop_index = 1000000000 % repeat
+print(loop[loop_index])
