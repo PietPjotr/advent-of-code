3205b9f500386e228c6ad805c71388e9b37c582b
PjotrPiet <Pjotrpiet@outlook.com>
2024-01-14 15:43:48 +0100

    ezpz zeker in de top 20 beland als ik mee had gedaan

---
+++ 2017/day_17/17.py
@@ -0,0 +1,33 @@
+import sys
+sys.path.append('..')
+import my_parser as p
+
+L = p.input_as_lines('inputs/inp.txt')
+G = G = [[el for el in line] for line in L]
+R = len(G)
+C = len(G[0])
+
+inp = 354
+
+buff = [0]
+pos = 0
+for i in range(1, 2018):
+    pos = (pos + inp) % len(buff)
+    buff.insert(pos + 1, i)
+    pos += 1
+
+print(buff[buff.index(2017) + 1])
+
+pos = 0
+buff = [-1 for _ in range(50000001)]
+buff[0] = 0
+length = 1
+for i in range(1, 50000001):
+    pos = (pos + inp) % length
+    buff[pos + 1] = i
+    pos += 1
+    length += 1
+    if i % 5000000 == 0:
+        print(str(i / 50000000 * 100) + '%')
+
+print(buff[buff.index(0) + 1])
