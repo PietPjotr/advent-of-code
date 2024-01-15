0f47ee9f8c0acd8ef736c2423e0697199f6daa47
PjotrPiet <Pjotrpiet@outlook.com>
2024-01-15 17:18:36 +0100

    24 dit keer wel

--- 2017/day_24/24.py
+++ 2017/day_24/24.py
@@ -1,8 +1,44 @@
 import sys
 sys.path.append('..')
 import my_parser as p
+from copy import deepcopy
 
 L = p.input_as_lines('inputs/inp.txt')
-G = G = [[el for el in line] for line in L]
-R = len(G)
-C = len(G[0])
+
+ports = [[int(el) for el in l.split('/')] for l in L]
+
+
+def p1():
+    starting_ports = [port for port in ports if 0 in port]
+    max_steps = 0
+    p1 = 0
+    p2 = 0
+    for port in starting_ports:
+        used = set()
+        used.add(tuple(port))
+        stack = [(0, sum(port), 0, port, used)]
+        while stack:
+            steps, strength, prev, port, used = stack.pop()
+            over = [el for el in port]
+            over.remove(prev)
+            over = over[0]
+            available_ports = [p for p in ports if over in p and tuple(p) not in used]
+            if not available_ports:
+                if strength > p1:
+                    p1 = strength
+                if steps >= max_steps:
+                    max_steps = steps
+                    if strength > p2:
+                        p2 = strength
+                continue
+
+            for new_port in available_ports:
+                new_used = deepcopy(used)
+                new_used.add(tuple(new_port))
+                stack.append((steps + 1, strength + sum(new_port), over, new_port, new_used))
+
+    print(p1)
+    print(p2)
+
+
+p1()
