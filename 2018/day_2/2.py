c976757ccd8d9359a71f6d513d42596190b33a1d
PjotrPiet <Pjotrpiet@outlook.com>
2024-01-17 15:06:59 +0100

    pretty easy duurde wel iets te lang door een stomme implementatie error

---
+++ 2018/day_2/2.py
@@ -0,0 +1,45 @@
+import sys
+sys.path.append('..')
+import my_parser as p
+
+L = p.input_as_lines('inputs/inp.txt')
+
+
+def p1():
+    two = 0
+    three = 0
+    for el in L:
+        el = [(char, el.count(char)) for char in el]
+        Two = True
+        Three = True
+        for char, c in el:
+            if c == 3 and Three:
+                three += 1
+                Three = False
+            if c == 2 and Two:
+                two += 1
+                Two = False
+
+    return three * two
+
+
+def p2():
+    for i in range(len(L)):
+        for j in range(len(L)):
+            el1 = L[i]
+            el2 = L[j]
+            same = 0
+            for k in range(len(el1)):
+                if el1[k] == el2[k]:
+                    same += 1
+
+            if same == len(el1) - 1:
+                ret = ''
+                for i in range(len(el1)):
+                    if el1[i] == el2[i]:
+                        ret += el1[i]
+                return ret
+
+
+print(p1())
+print(p2())
